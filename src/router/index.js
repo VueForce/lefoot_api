221922b0cf36b5c7e2f1a8d8769a1b182a2015ef
Davi Ramos <davecadastro@gmail.com>
2022-04-14 00:28:51 -0300

    Add axios, refactoring routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,41 +4,41 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/AboutView.vue')
+    // },
+    // {
+    //   path: '/redefinir',
+    //   name: 'Redefinir',
+    //   component: () => import('../views/RedefinirSenhaView.vue')
+    // },
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/redefinir-senha',
-      name: 'redefinir-senha',
-      component: () => import('../views/RedefinirSenhaView.vue')
-    },
-    {
-      path: '/tela-csv',
-      name: 'tela-csv',
+      name: 'Csv',
       component: () => import('../views/TelaCsvView.vue')
     },
-    {
-      path: '/criar-conta',
-      name: 'criar-conta',
-      component: () => import('../views/CriarContaView.vue')
-    },
-    {
-      path: '/tela-login',
-      name: 'tela-login',
-      component: () => import('../views/TelaLoginView.vue')
-    },
-    {
-      path: '/filtrar-clientes',
-      name: 'filtrar-clientes',
-      component: () => import('../views/FiltrarClientesView.vue')
-    },
+    // {
+    //   path: '/new/account',
+    //   name: 'Conta',
+    //   component: () => import('../views/CriarContaView.vue')
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'Login',
+    //   component: () => import('../views/TelaLoginView.vue')
+    // },
+    // {
+    //   path: '/filtrar-clientes',
+    //   name: 'filtrar-clientes',
+    //   component: () => import('../views/FiltrarClientesView.vue')
+    // },
   ]
 })
 
--- src/views/TelaCsvView.vue
+++ src/views/TelaCsvView.vue
@@ -4,14 +4,13 @@
 
     <form>
         <div style="display: flex; justify-content: center;">
-            <label for="arquivo">Insira arquivo aqui</label>
-            <input type="file" name="arquivo" id="arquivo" accept=".csv">
+            <label for="arquivo" >Insira arquivo aqui</label>
+            <input type="file" @change="uploadFile" ref="file" name="arquivo" id="arquivo" accept=".csv">
         </div>
     </form>
       <div style="display: grid; grid-template-columns: 1fr 1fr; grid-column-gap: 32px;">
         <Button style="margin-top: 10px">Voltar</Button>
-        <Button style="margin-top: 10px">Carregar</Button>
-
+        <Button style="margin-top: 10px" @click="submitFile">Enviar</Button>
       </div>
   </div>
 </template>
@@ -51,8 +50,7 @@ input {
 input[type="file"] {
     display: none;
 }
-label {
-}
+
 label:hover {
   font-weight: 700;
   letter-spacing: 0.5px;
@@ -60,6 +58,7 @@ label:hover {
   background-color: #cfcfcf;
   color:#2c3e50;
 }
+
 label {
     font-weight: 700;
     letter-spacing: 0.5px;
@@ -76,3 +75,34 @@ label {
     border: 2px dashed rgba(0, 0, 0, 0.623);
 }
 </style>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: () => ({file: null, fileName: ''}),
+    methods: {
+      uploadFile() {
+        this.file = this.$refs.file.files[0];
+        this.fileName = this.$refs.file.files[0].name;
+      },
+      submitFile() {
+        const formData = new FormData();
+        
+        formData.append('file', this.file);
+
+        const headers = { 'Content-Type': 'multipart/form-data' };
+        
+        axios.post('http://localhost:8080/api/csv/upload', formData, { headers })
+        .then((res) => {
+          res.data.files;
+          res.status;
+
+          if(res.status === 200) {
+            alert(`O arquivo ${this.fileName} foi enviado com sucesso`);
+          }
+        });
+      }
+    }
+}
+</script>
